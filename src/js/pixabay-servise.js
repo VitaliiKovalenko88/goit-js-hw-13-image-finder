f6c2383160f2bf09219fa313cbc18c7d5d81b0d6
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-07-13 18:29:03 +0200

    доделал класс запроса

--- src/js/pixabay-servise.js
+++ src/js/pixabay-servise.js
@@ -11,17 +11,25 @@ export default class NewsImages {
     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
     return fetch(url)
       .then(response => response.json())
-      .then(date => {
-        return date
+      .then(({ hits }) => {
+        this.incrementPage()
+        return hits
       })
     
   }
+incrementPage() {
+    this.page += 1;
+  }
 
-  get(query) {
+   resetPage() {
+    this.page = 1;
+  }
+
+  get query () {
     return this.searchQuery
   }
 
-  set(newQuery) {
+  set query (newQuery) {
     this.searchQuery=newQuery
   }
 }
