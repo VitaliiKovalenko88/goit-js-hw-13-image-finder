9ef142a09c56f2c43a013c48a9a05e0894bbf28e
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-07-13 12:28:10 +0200

    добавил приход даных

--- src/js/pixabay-servise.js
+++ src/js/pixabay-servise.js
@@ -4,14 +4,16 @@ const BASE_URL = 'https://pixabay.com/api/'
 export default class NewsImages {
   constructor() {
     this.searchQuery = '';
-    this.page=1
+    this.page = 1;
   }
 
   fetchImages() {
     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
     return fetch(url)
       .then(response => response.json())
-    .then(console.log)
+      .then(date => {
+        return date
+      })
     
   }
 
