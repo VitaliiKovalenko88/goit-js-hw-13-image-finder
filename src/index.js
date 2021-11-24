ce0e7769e3dcdd0a1aa12bcb729eb552f3e5a194
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-11-24 20:49:37 +0100

    добавил стили

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import './sass/main.scss';
-import  NewsImages from './js/pixabay-servise.js'
+import NewsImages from './js/pixabay-servise.js';
 import { debounce } from 'lodash';
+import 'material-icons/iconfont/material-icons.css';
 const newImages = new NewsImages();
 
 const refs = {
   input: document.querySelector('.js-input'),
-  container: document.querySelector('.search-form'),
+  form: document.querySelector('.search-form'),
+  galleryEl: document.querySelector('.gallery'),
+  searchBtn: document.querySelector('#search-btn'),
+  loadBtn: document.querySelector('.load-more-btn'),
 };
 
-refs.input.addEventListener('input',debounce(onSearch, 500) )
-
-function onSearch(event) {
-  event.preventDefault();
+function onSearch(e) {
+  e.preventDefault();
   // clearCardContainer();
-  newImages.query = event.target.value;
+  newImages.query = e.currentTarget.elements.query.value;
+  console.log(newImages.query);
   // if (!newImages.query) {
   //   showInfo();
   //   return;
@@ -22,8 +25,54 @@ function onSearch(event) {
   fetchImages();
 }
 
-function fetchImages() {
+const clearGalleryContainer = () => {
+  refs.gallery.innerHTML = '';
+};
+
+const markupPhoto = ({
+  webformatURL,
+  likes,
+  views,
+  comments,
+  downloads,
+  largeImageURL,
+  tags,
+}) => {
+  return `<li class="images-item">
+            <div class="photo-card">
+              <img src="${webformatURL}" alt="${tags}" width="360" height="250" data-large-img-src="${largeImageURL}" />
+                <div class="stats">
+                 <p class="stats-item">
+                  <i class="material-icons">thumb_up</i>
+                  ${likes}
+                 </p>
+                 <p class="stats-item">
+                  <i class="material-icons">visibility</i>
+                   ${views}
+                 </p>
+                 <p class="stats-item">
+                  <i class="material-icons">comment</i>
+                   ${comments}
+                 </p>
+                 <p class="stats-item">
+                  <i class="material-icons">cloud_download</i>
+                   ${downloads}
+                 </p>
+                </div>
+            </div>
+          </li>`;
+};
+
+const createMarkupGallery = img => {
+  const gallery = img.map(markupPhoto).join();
+  refs.galleryEl.insertAdjacentHTML('afterbegin', gallery);
+};
+
+const fetchImages = () => {
   newImages.fetchImages().then(data => {
+    createMarkupGallery(data);
     console.log(data);
-  })
-}
+  });
+};
+
+refs.form.addEventListener('submit', debounce(onSearch, 500));
--- src/js/pixabay-servise.js
+++ src/js/pixabay-servise.js
@@ -1,5 +1,5 @@
-const API_KEY = '22469389-b601f8ee0455705c879309776'
-const BASE_URL = 'https://pixabay.com/api/'
+const API_KEY = '22469389-b601f8ee0455705c879309776';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export default class NewsImages {
   constructor() {
@@ -10,26 +10,27 @@ export default class NewsImages {
   fetchImages() {
     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
     return fetch(url)
-      .then(response => response.json())
-      .then(({ hits }) => {
-        this.incrementPage()
-        return hits
+      .then(response => {
+        return response.json();
       })
-    
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
   }
-incrementPage() {
+  incrementPage() {
     this.page += 1;
   }
 
-   resetPage() {
+  resetPage() {
     this.page = 1;
   }
 
-  get query () {
-    return this.searchQuery
+  get query() {
+    return this.searchQuery;
   }
 
-  set query (newQuery) {
-    this.searchQuery=newQuery
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
